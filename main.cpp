21379d2c16c89891ea423eb3546e826ec5453bb2
Androu <15367779+AndreasRapatas@users.noreply.github.com>
2018-02-13 12:42:38 +0200

    Remove code duplication, test with 4 dimentions

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ template<typename T>
 class Matrix {
 
 	vector<unsigned> dimentions;
-	vector<unsigned> temp;
 	vector<T> data;
 
 public:
 
 	template<typename... T_dim>
 	Matrix(T_dim... dimz) {
+
 		dimentions = { static_cast<unsigned>(dimz)... };
 
 		unsigned size = 1;
@@ -28,7 +28,9 @@ public:
 	unsigned size() const { return dimentions.size(); }
 
 	template<typename... T_dim>
-	T const& operator()(T_dim... dimz) const {
+	unsigned find_index(T_dim... dimz) const {
+
+		vector<unsigned> temp;
 		temp = { static_cast<unsigned>(dimz)... };
 
 		unsigned index = 0;
@@ -43,26 +45,19 @@ public:
 			index += multiplier * temp[i];
 		}
 
-		return data[index];
+		return index;
+	}
+
+	template<typename... T_dim>
+	T const& operator()(T_dim... dimz) const {
+
+		return data[find_index(dimz...)];
 	}
 
 	template<typename... T_dim>
 	T &operator()(T_dim... dimz) {
-		temp = { static_cast<unsigned>(dimz)... };
 
-		unsigned index = 0;
-
-		for (size_t i = 0; i < size(); ++i) {
-
-			unsigned multiplier = 1;
-			for (size_t dimention_index = 0; dimention_index < i; ++dimention_index) {
-				multiplier *= dimentions[dimention_index];
-			}
-
-			index += multiplier * temp[i];
-		}
-
-		return data[index];
+		return data[find_index(dimz...)];
 	}
 
 	void list_dimentions() {
@@ -80,31 +75,34 @@ int main() {
 	unsigned width = 4;
 	unsigned height = 3;
 	unsigned depth = 4;
+	unsigned time = 20;
 
-	Matrix<char> a(width, height, depth);
+	Matrix<char> a(width, height, depth, time);
 
-	a.list_dimentions();
-
-	for (size_t z = 0; z < depth; ++z) {
-		for (size_t y = 0; y < height; ++y) {
-			for (size_t x = 0; x < width; ++x) {
-				a(x, y, z) = '#';
+	for (size_t t = 0; t < time; ++t) {
+		for (size_t z = 0; z < depth; ++z) {
+			for (size_t y = 0; y < height; ++y) {
+				for (size_t x = 0; x < width; ++x) {
+					a(x, y, z, t) = '#';
+				}
 			}
 		}
 	}
 
-	a(2, 1, 3) = '_';
+	a(2, 1, 3, 18) = '_';
 
-	for (size_t z = 0; z < depth; ++z) {
-		for (size_t y = 0; y < height; ++y) {
-			for (size_t x = 0; x < width; ++x) {
-				cout << a(x, y, z) << ' ';
+	for (size_t t = 0; t < time; ++t) {
+		for (size_t z = 0; z < depth; ++z) {
+			for (size_t y = 0; y < height; ++y) {
+				for (size_t x = 0; x < width; ++x) {
+					cout << a(x, y, z, t) << ' ';
+				}
+				cout << '\n';
 			}
-			cout << '\n';
+			cout << "=======\n";
 		}
-		cout << "====\n";
+		cout << "\n\n\n";
 	}
-
 
 	return 0;
 }
