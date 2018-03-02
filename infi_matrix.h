9b9e01eb4c8e479fe0b632152d8c5d83a1d776ea
Androu <15367779+AndreasRapatas@users.noreply.github.com>
2018-03-02 09:26:13 +0200

    Removed vector backend

--- infi_matrix.h
+++ infi_matrix.h
@@ -1,39 +1,59 @@
 #ifndef INFI_MATRIX_H
 #define INFI_MATRIX_H
 
-#include <vector>
-
 template<typename T>
 class infi_matrix {
 
-	std::vector<unsigned> dimentions;
-	std::vector<T> data;
+	unsigned *dimentions;
+	unsigned dimention_count;
+	T *data;
+	unsigned data_size;
+
+	template<typename... Type>
+	unsigned *array_from_variadic(Type... args) const {
+
+		unsigned *ret = new unsigned[sizeof...(args)];
+
+		unsigned i = 0;
+		for (const unsigned& num : {args...}) {
+			ret[i] = num;
+			++i;
+		}
+
+		return ret;
+	}
 
 public:
 
 	template<typename... T_dim>
-	infi_matrix(T_dim... dimz) {
-
-		dimentions = { static_cast<unsigned>(dimz)... };
+	infi_matrix(T_dim... dimz) :
+		dimention_count(sizeof...(T_dim))
+	{
+		dimentions = array_from_variadic(dimz...);
 
 		unsigned size = 1;
-		for (auto d : dimentions) {
-			size *= d;
+		for (unsigned i = 0; i < dimention_count; ++i) {
+			size *= dimentions[i];
 		}
-		data.reserve(size);
+		data = new T[size];
 	}
 
-	unsigned size() const { return dimentions.size(); }
+	~infi_matrix() {
+
+		delete[] dimentions;
+		delete[] data;
+	}
+
+	unsigned size() const { return data_size; }
 
 	template<typename... T_dim>
 	unsigned find_index(T_dim... dimz) const {
 
-		std::vector<unsigned> temp;
-		temp = { static_cast<unsigned>(dimz)... };
+		unsigned *provided_dimz = array_from_variadic(dimz...);
 
 		unsigned index = 0;
 
-		for (unsigned i = 0; i < size(); ++i) {
+		for (unsigned i = 0; i < sizeof...(T_dim); ++i) {
 
 			unsigned multiplier = 1;
 			for (
@@ -44,31 +64,29 @@ public:
 				multiplier *= dimentions[dimention_index];
 			}
 
-			index += multiplier * temp[i];
+			index += multiplier * provided_dimz[i];
 		}
+
+		delete[] provided_dimz;
 
 		return index;
 	}
 
 	template<typename... T_dim>
 	T const& operator()(T_dim... dimz) const {
-
 		return data[find_index(dimz...)];
 	}
 
 	template<typename... T_dim>
 	T &operator()(T_dim... dimz) {
-
 		return data[find_index(dimz...)];
 	}
 
 	T const& operator[](unsigned index) const {
-
 		return data[index];
 	}
 
 	T &operator[](unsigned index) {
-
 		return data[index];
 	}
 };
